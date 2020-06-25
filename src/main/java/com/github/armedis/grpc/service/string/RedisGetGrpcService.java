041f39971be82f8c31ace3d549a8b6bd5b8ae875
krisjey <krisjey@kris-PC.office.afreecatv.com>
2020-06-25 18:54:31 +0900

    Add grpc service for redis get command.

---
+++ src/main/java/com/github/armedis/grpc/service/string/RedisGetGrpcService.java
@@ -0,0 +1,53 @@
+
+package com.github.armedis.grpc.service.string;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.github.armedis.grpc.service.string.RedisStringServiceGrpc.RedisStringServiceImplBase;
+import com.github.armedis.http.service.ResponseCode;
+import com.github.armedis.redis.command.RedisCommandExecuteResult;
+
+import io.grpc.stub.StreamObserver;
+
+/**
+ * Redis get http request endpoint service.
+ * @author krisjey
+ *
+ */
+@Service
+public class RedisGetGrpcService extends RedisStringServiceImplBase {
+    private static final Logger logger = LoggerFactory.getLogger(RedisGetGrpcService.class);
+
+    @Override
+    public void get(GetRequest getRequest, StreamObserver<GetResponse> responseObserver) {
+
+        // client 어떻게 생성하나?
+
+        // 실행부..
+        // converter 필요.
+//        // execute redis command by http request params.
+//        RedisCommandExecuteResult result = null;
+//        try {
+//            result = executeCommand(redisRequest);
+//        }
+//        catch (Exception e) {
+//            logger.error("Can not execute redis command ", e);
+//            return buildResponse(ResponseCode.UNKNOWN_ERROR, redisRequest);
+//        }
+
+//        Redisgrpc
+        logger.info("Unary message " + getRequest.getKey());
+        GetResponse response = GetResponse.newBuilder()
+                .setCode("200")
+                .setResult("hello world " + getRequest.getKey())
+                .build();
+
+        // Server Streaming이면 responseObserver.onNext()를 두 번 이상 호출할 수 있다.
+        responseObserver.onNext(response);
+
+        // 응답 완료
+        responseObserver.onCompleted();
+    }
+}
