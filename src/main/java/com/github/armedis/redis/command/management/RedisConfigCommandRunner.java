2f4cbf39c96a32287fc995533722afe84be632b2
krisjey <smufu@naver.com>
2024-03-27 20:17:47 +0900

    Change package name.

--- src/main/java/com/github/armedis/redis/command/management/RedisConfigCommandRunner.java
+++ src/main/java/com/github/armedis/redis/command/management/RedisConfigCommandRunner.java
@@ -1,5 +1,5 @@
 
-package com.github.armedis.redis.command.config;
+package com.github.armedis.redis.command.management;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -50,13 +50,11 @@ public class RedisConfigCommandRunner extends AbstractRedisCommandRunner {
 
         return RedisCommandExecuteResultFactory.buildRedisCommandExecuteResult(result);
     }
-    
-    // TODO get인지 set인지 구분.
+
+    // TODO sub command를 get인지 set인지 구분.
     @Override
     public RedisCommandExecuteResult executeAndGet(RedisClusterCommands<String, String> commands) {
         logger.info(redisRequest.toString());
-
-//     TODO  위로 올리던지. 서버 그룹별로 명령어를 던지던지 아니면 loopup에서 가져오던지.
 
         String key = this.redisRequest.getKey();
         String value = this.redisRequest.getValue();
@@ -100,42 +98,7 @@ public class RedisConfigCommandRunner extends AbstractRedisCommandRunner {
     }
 
     private RedisClusterWideCommand getRedisClusterWideCommandMode(String subCommand) {
-        return redisClusterWideCommandSet.get(subCommand);
+        return RedisClusterWideCommands.get(subCommand);
     }
 
-    private static final Map<String, RedisClusterWideCommand> redisClusterWideCommandSet = new HashMap<String, RedisClusterWideCommand>();
-    static {
-        // memory management
-        redisClusterWideCommandSet.put("activedefrag", RedisClusterWideCommand.ALL); // no
-
-        // server memory
-        redisClusterWideCommandSet.put("maxmemory-policy", RedisClusterWideCommand.ALL); // volatile-lru, volatile-lfu, volatile-random, volatile-ttl, allkeys-lru, allkeys-lfu, allkeys-random, noeviction
-        redisClusterWideCommandSet.put("maxmemory-samples", RedisClusterWideCommand.ALL); // "5"
-        redisClusterWideCommandSet.put("maxmemory", RedisClusterWideCommand.ALL); // byte
-
-        // clients
-        redisClusterWideCommandSet.put("timeout", RedisClusterWideCommand.ALL); // "0"
-        redisClusterWideCommandSet.put("maxclients", RedisClusterWideCommand.ALL); // "4064"
-
-        // snapshot
-        redisClusterWideCommandSet.put("save", RedisClusterWideCommand.ALL); // 3600 1 300 100 60 10000
-        redisClusterWideCommandSet.put("appendonly", RedisClusterWideCommand.ALL); // "no"
-
-        // lazyfree
-        redisClusterWideCommandSet.put("lazyfree-lazy-eviction", RedisClusterWideCommand.ALL); // "no" recommend yes
-        redisClusterWideCommandSet.put("lazyfree-lazy-expire", RedisClusterWideCommand.ALL); // "no" recommend yes
-        redisClusterWideCommandSet.put("lazyfree-lazy-server-del", RedisClusterWideCommand.ALL); // "no" recommend yes
-        redisClusterWideCommandSet.put("lazyfree-lazy-user-del", RedisClusterWideCommand.ALL); // "no" recommend yes
-        redisClusterWideCommandSet.put("lazyfree-lazy-user-flush", RedisClusterWideCommand.ALL); // "no" recommend yes
-
-        // time limit
-        redisClusterWideCommandSet.put("lua-time-limit", RedisClusterWideCommand.ALL); // "5000"
-        redisClusterWideCommandSet.put("cluster-node-timeout", RedisClusterWideCommand.ALL); // "15000"
-        redisClusterWideCommandSet.put("slowlog-log-slower-than", RedisClusterWideCommand.ALL); // "10000"
-
-        // logging
-        redisClusterWideCommandSet.put("syslog-ident", RedisClusterWideCommand.ALL); // "redis"
-        redisClusterWideCommandSet.put("loglevel", RedisClusterWideCommand.ALL); // "notice"
-        redisClusterWideCommandSet.put("appendfsync", RedisClusterWideCommand.ALL); // "everysec"
-    }
 }
