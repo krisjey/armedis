8bef42f4044644cf102031b918beb440c8cca8ff
krisjey <smufu@naver.com>
2025-08-27 18:40:11 +0900

    dummy commit.

---
+++ src/main/java/com/github/armedis/redis/command/management/RedisSlowlogRequest.java
@@ -0,0 +1,29 @@
+
+package com.github.armedis.redis.command.management;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.armedis.http.service.request.RedisRequest;
+import com.linecorp.armeria.common.AggregatedHttpRequest;
+import com.linecorp.armeria.server.annotation.Param;
+
+public class RedisSlowlogRequest extends RedisRequest {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    @Param("value")
+    protected String value;
+
+    public RedisSlowlogRequest(AggregatedHttpRequest httpRequest) {
+        super(httpRequest);
+        this.setCommand("Config");
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
