242dbfbb8606f3391a75db531e92976ad31f3e5c
krisjey <smufu@naver.com>
2025-10-09 16:37:04 +0900

    Add /actuator test with JsonFluentAssert

---
+++ src/test/java/com/github/armedis/http/service/management/ArmedisActuatorTest.java
@@ -0,0 +1,47 @@
+
+package com.github.armedis.http.service.management;
+
+import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.github.armedis.ArmedisServer;
+import com.github.armedis.http.service.BaseServiceTest;
+import com.linecorp.armeria.common.AggregatedHttpResponse;
+import com.linecorp.armeria.common.HttpStatus;
+
+@ActiveProfiles("testbed")
+@SpringBootTest(webEnvironment = WebEnvironment.NONE, classes = ArmedisServer.class)
+public class ArmedisActuatorTest extends BaseServiceTest {
+    @Test
+    void testActuator() throws JsonParseException, JsonMappingException, IOException {
+        AggregatedHttpResponse res = null;
+        String response = null;
+
+        // spring actuator health 체크.
+        res = client.get("/actuator").aggregate().join();
+        assertThat(res.status()).isEqualTo(HttpStatus.OK);
+
+        response = res.content().toStringUtf8();
+
+        assertThatJson(response)
+                .as("Check requestUrl of result")
+                .node("_links").isPresent();
+
+        assertThatJson(response)
+                .as("Check requestUrl of result")
+                .node("_links.self.href").isPresent();
+
+        assertThatJson(response)
+                .as("Check requestUrl of result")
+                .node("_links.self.href").isEqualTo("/actuator");
+    }
+}
