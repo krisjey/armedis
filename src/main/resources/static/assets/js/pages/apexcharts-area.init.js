7e5d67c93cdcecf4481f9cdef890fbc265937da8
홍태기 가치플레이 <20181010@hyundaidept.com>
2025-10-16 10:01:10 +0900

    수정
    - overview
      - client connections
      - slow queries log table
      - 브라우저 닫거나 내렸을 때 10초뒤 차트 재 랜더링
    
    - realtime stats
      - 브라우저 닫거나 내렸을 때 10초뒤 차트 재 랜더링
      - null 처리 버그수정
    
    - management
      - 로그인팝업 추가

--- src/main/resources/static/assets/js/pages/apexcharts-area.init.js
+++ src/main/resources/static/assets/js/pages/apexcharts-area.init.js
@@ -9,27 +9,29 @@ File: Area Chart init js
 // get colors array from the string
 function getChartColorsArray(chartId) {
     var parent = document.getElementById(chartId);
-    var chartElement = parent.querySelector("#area_chart_stacked");
-
-    if (chartElement !== null) {
-        var colors = chartElement.getAttribute("data-colors");
-        colors = JSON.parse(colors);
-        return colors.map(function (value) {
-            var newValue = value.replace(" ", "");
-            if (newValue.indexOf(",") === -1) {
-                var color = getComputedStyle(document.documentElement).getPropertyValue(newValue);
-                if (color) return color;
-                else return newValue;
-            } else {
-                var val = value.split(',');
-                if (val.length == 2) {
-                    var rgbaColor = getComputedStyle(document.documentElement).getPropertyValue(val[0]);
-                    rgbaColor = "rgba(" + rgbaColor + "," + val[1] + ")";
-                    return rgbaColor;
+    if (parent) {
+        var chartElement = parent.querySelector("#area_chart_stacked");
+    
+        if (chartElement !== null) {
+            var colors = chartElement.getAttribute("data-colors");
+            colors = JSON.parse(colors);
+            return colors.map(function (value) {
+                var newValue = value.replace(" ", "");
+                if (newValue.indexOf(",") === -1) {
+                    var color = getComputedStyle(document.documentElement).getPropertyValue(newValue);
+                    if (color) return color;
+                    else return newValue;
                 } else {
-                    return newValue;
+                    var val = value.split(',');
+                    if (val.length == 2) {
+                        var rgbaColor = getComputedStyle(document.documentElement).getPropertyValue(val[0]);
+                        rgbaColor = "rgba(" + rgbaColor + "," + val[1] + ")";
+                        return rgbaColor;
+                    } else {
+                        return newValue;
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 }
