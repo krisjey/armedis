195f18bbdc7fcac21e55d499b05cd4b279c7f3cb
krisjey <smufu@naver.com>
2025-10-23 20:39:38 +0900

    change test suite @SelectPackages instead @SelectClasses

--- src/test/java/com/github/armedis/http/service/management/ServiceTestSuite.java
+++ src/test/java/com/github/armedis/http/service/management/ServiceTestSuite.java
@@ -1,16 +1,24 @@
 package com.github.armedis.http.service.management;
 
-import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({
-        ArmedisActuatorTest.class,
-        RedisClientListServiceTest.class,
-        RedisConfigServiceTest.class,
-        RedisConfigsServiceTest.class,
-        RedisLoginServiceTest.class,
-})
+@SelectPackages("com.github.armedis.http.service.management")  // 기준 패키지
+@IncludeClassNamePatterns(".*Test")     // (선택) 클래스 이름 패턴 지정
 public class ServiceTestSuite {
-    
+    /** 
+     * activedefrag
+     * maxmemory-policy
+     * maxmemory-samples
+     * maxmemory
+     * timeout
+     * maxclients
+     * save
+     * appendonly
+     * lazyfree-lazy-expire
+     * lazyfree-lazy-eviction
+     * lazyfree-lazy-server-del
+     */ 
 }
