46c26abaa1658a8546161dd071a0c4d492ff9b5f
krisjey <smufu@naver.com>
2025-10-23 20:40:54 +0900

    Remove unused comment and formatting.

--- src/main/java/com/github/armedis/redis/command/management/RedisClusterWideCommands.java
+++ src/main/java/com/github/armedis/redis/command/management/RedisClusterWideCommands.java
@@ -15,41 +15,39 @@ public class RedisClusterWideCommands {
     private static final Map<String, RedisClusterWideCommand> redisClusterWideCommandSet = new HashMap<String, RedisClusterWideCommand>();
     static {
         // management/server
-        redisClusterWideCommandSet.put("save", RedisClusterWideCommand.ALL); // no
-        redisClusterWideCommandSet.put("bgsave", RedisClusterWideCommand.ALL); // no
-        
+        redisClusterWideCommandSet.put("save", RedisClusterWideCommand.ALL);
+        redisClusterWideCommandSet.put("bgsave", RedisClusterWideCommand.ALL);
+
         // memory management
-        redisClusterWideCommandSet.put("activedefrag", RedisClusterWideCommand.ALL); // no
+        redisClusterWideCommandSet.put("activedefrag", RedisClusterWideCommand.ALL);
 
         // server memory
-        redisClusterWideCommandSet.put("maxmemory-policy", RedisClusterWideCommand.ALL); // volatile-lru, volatile-lfu, volatile-random, volatile-ttl, allkeys-lru, allkeys-lfu, allkeys-random, noeviction
-        redisClusterWideCommandSet.put("maxmemory-samples", RedisClusterWideCommand.ALL); // "5"
-        redisClusterWideCommandSet.put("maxmemory", RedisClusterWideCommand.ALL); // byte
+        redisClusterWideCommandSet.put("maxmemory", RedisClusterWideCommand.ALL);
+        redisClusterWideCommandSet.put("maxmemory-policy", RedisClusterWideCommand.ALL);
+        redisClusterWideCommandSet.put("maxmemory-samples", RedisClusterWideCommand.ALL);
 
         // clients
-        redisClusterWideCommandSet.put("timeout", RedisClusterWideCommand.ALL); // "0"
-        redisClusterWideCommandSet.put("maxclients", RedisClusterWideCommand.ALL); // "4064"
+        redisClusterWideCommandSet.put("timeout", RedisClusterWideCommand.ALL);
+        redisClusterWideCommandSet.put("maxclients", RedisClusterWideCommand.ALL);
 
         // snapshot
-        redisClusterWideCommandSet.put("save", RedisClusterWideCommand.ALL); // 3600 1 300 100 60 10000
-        redisClusterWideCommandSet.put("appendonly", RedisClusterWideCommand.ALL); // "no"
+        redisClusterWideCommandSet.put("save", RedisClusterWideCommand.ALL);
+        redisClusterWideCommandSet.put("appendonly", RedisClusterWideCommand.ALL);
 
         // lazyfree
-        redisClusterWideCommandSet.put("lazyfree-lazy-eviction", RedisClusterWideCommand.ALL); // "no" recommend yes
-        redisClusterWideCommandSet.put("lazyfree-lazy-expire", RedisClusterWideCommand.ALL); // "no" recommend yes
-        redisClusterWideCommandSet.put("lazyfree-lazy-server-del", RedisClusterWideCommand.ALL); // "no" recommend yes
-        redisClusterWideCommandSet.put("lazyfree-lazy-user-del", RedisClusterWideCommand.ALL); // "no" recommend yes
-        redisClusterWideCommandSet.put("lazyfree-lazy-user-flush", RedisClusterWideCommand.ALL); // "no" recommend yes
+        redisClusterWideCommandSet.put("lazyfree-lazy-eviction", RedisClusterWideCommand.ALL);
+        redisClusterWideCommandSet.put("lazyfree-lazy-expire", RedisClusterWideCommand.ALL);
+        redisClusterWideCommandSet.put("lazyfree-lazy-server-del", RedisClusterWideCommand.ALL);
+        redisClusterWideCommandSet.put("lazyfree-lazy-user-del", RedisClusterWideCommand.ALL);
+        redisClusterWideCommandSet.put("lazyfree-lazy-user-flush", RedisClusterWideCommand.ALL);
 
         // time limit
-        redisClusterWideCommandSet.put("lua-time-limit", RedisClusterWideCommand.ALL); // "5000"
-        redisClusterWideCommandSet.put("cluster-node-timeout", RedisClusterWideCommand.ALL); // "15000"
-        redisClusterWideCommandSet.put("slowlog-log-slower-than", RedisClusterWideCommand.ALL); // "10000"
+        redisClusterWideCommandSet.put("lua-time-limit", RedisClusterWideCommand.ALL);
+        redisClusterWideCommandSet.put("slowlog-log-slower-than", RedisClusterWideCommand.ALL);
 
         // logging
-        redisClusterWideCommandSet.put("syslog-ident", RedisClusterWideCommand.ALL); // "redis"
-        redisClusterWideCommandSet.put("loglevel", RedisClusterWideCommand.ALL); // "notice"
-        redisClusterWideCommandSet.put("appendfsync", RedisClusterWideCommand.ALL); // "everysec"
+        redisClusterWideCommandSet.put("loglevel", RedisClusterWideCommand.ALL);
+        redisClusterWideCommandSet.put("appendfsync", RedisClusterWideCommand.ALL);
     }
 
     /**
