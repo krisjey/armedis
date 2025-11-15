a43dc2c89f74a936672a99451c59ce2b5e708113
krisjey <krisjey@kris-home-5600>
2025-11-15 19:50:07 +0900

    Remove unnecessary code.

--- src/test/java/com/github/armedis/ArmedisServerConfigurationTest.java
+++ src/test/java/com/github/armedis/ArmedisServerConfigurationTest.java
@@ -3,9 +3,6 @@ package com.github.armedis;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Random;
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
