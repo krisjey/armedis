3b524d9529d3500771f0c9f5678a8d9d838cdbb3
krisjey <smufu@naver.com>
2025-12-19 16:24:21 +0900

    Change private method name.

--- src/test/java/com/github/armedis/redis/connection/RedisServerDetectorTest.java
+++ src/test/java/com/github/armedis/redis/connection/RedisServerDetectorTest.java
@@ -20,28 +20,28 @@ class RedisServerDetectorTest {
     @Test
     void singleNodeTest() {
         // single node 6379
-        checkNodes("singleTest", "192.168.56.105", 6379, "", 1, 1, 0);
+        checkAndCreateDetector("singleTest", "192.168.56.105", 6379, "", 1, 1, 0);
     }
 
     @Test
     void replicaNodeTest1() {
         // 1 master 2 slaves 8000, 8101, 8102
-        checkNodes("replicaTest", "192.168.56.105", 8000, "", 3, 1, 2);
-        checkNodes("replicaTest", "192.168.56.105", 8101, "", 3, 1, 2);
-        checkNodes("replicaTest", "192.168.56.105", 8102, "", 3, 1, 2);
+        checkAndCreateDetector("replicaTest", "192.168.56.105", 8000, "", 3, 1, 2);
+        checkAndCreateDetector("replicaTest", "192.168.56.105", 8101, "", 3, 1, 2);
+        checkAndCreateDetector("replicaTest", "192.168.56.105", 8102, "", 3, 1, 2);
     }
 
     @Test
     void sentinelDataNodeTest() {
         RedisServerDetector redisServerDetector = null;
         // 1 master 2 slaves 8500, 8501, 8502
-        redisServerDetector = checkNodes("sentinelDataTest", "192.168.56.105", 8500, "", 3, 1, 2);
+        redisServerDetector = checkAndCreateDetector("sentinelDataTest", "192.168.56.105", 8500, "", 3, 1, 2);
         System.out.println("sentinelDataNodeTest " + redisServerDetector.getAllNodes().toString());
 
-        redisServerDetector = checkNodes("sentinelDataTest", "192.168.56.105", 8501, "", 3, 1, 2);
+        redisServerDetector = checkAndCreateDetector("sentinelDataTest", "192.168.56.105", 8501, "", 3, 1, 2);
         System.out.println("sentinelDataNodeTest " + redisServerDetector.getAllNodes().toString());
 
-        redisServerDetector = checkNodes("sentinelDataTest", "192.168.56.105", 8502, "", 3, 1, 2);
+        redisServerDetector = checkAndCreateDetector("sentinelDataTest", "192.168.56.105", 8502, "", 3, 1, 2);
         System.out.println("sentinelNodeTest All " + redisServerDetector.getAllNodes().toString());
     }
 
@@ -49,30 +49,30 @@ class RedisServerDetectorTest {
     void sentinelNodeTest() {
         RedisServerDetector redisServerDetector = null;
         // 1 master 2 slaves 8500, 8501, 8502
-        redisServerDetector = checkNodes("sentinelTest", "192.168.56.105", 26379, "", 3, 1, 2);
+        redisServerDetector = checkAndCreateDetector("sentinelTest", "192.168.56.105", 26379, "", 3, 1, 2);
         System.out.println("sentinelNodeTest All " + redisServerDetector.getAllNodes().toString());
         System.out.println("sentinelNodeTest Sen " + redisServerDetector.getSentinelNodes().toString());
 
-        redisServerDetector = checkNodes("sentinelTest", "192.168.56.105", 26380, "", 3, 1, 2);
+        redisServerDetector = checkAndCreateDetector("sentinelTest", "192.168.56.105", 26380, "", 3, 1, 2);
         System.out.println("sentinelNodeTest All " + redisServerDetector.getAllNodes().toString());
         System.out.println("sentinelNodeTest Sen " + redisServerDetector.getSentinelNodes().toString());
 
-        redisServerDetector = checkNodes("sentinelTest", "192.168.56.105", 26381, "", 3, 1, 2);
+        redisServerDetector = checkAndCreateDetector("sentinelTest", "192.168.56.105", 26381, "", 3, 1, 2);
         System.out.println("sentinelNodeTest All " + redisServerDetector.getAllNodes().toString());
         System.out.println("sentinelNodeTest Sen " + redisServerDetector.getSentinelNodes().toString());
     }
 
     @Test
     void clusterNodeTest() {
-        checkNodes("clusterTest", "192.168.56.105", 17001, "", 6, 3, 3);
-        checkNodes("clusterTest", "192.168.56.105", 17002, "", 6, 3, 3);
-        checkNodes("clusterTest", "192.168.56.105", 17003, "", 6, 3, 3);
-        checkNodes("clusterTest", "192.168.56.105", 17004, "", 6, 3, 3);
-        checkNodes("clusterTest", "192.168.56.105", 17005, "", 6, 3, 3);
-        checkNodes("clusterTest", "192.168.56.105", 17006, "", 6, 3, 3);
+        checkAndCreateDetector("clusterTest", "192.168.56.105", 17001, "", 6, 3, 3);
+        checkAndCreateDetector("clusterTest", "192.168.56.105", 17002, "", 6, 3, 3);
+        checkAndCreateDetector("clusterTest", "192.168.56.105", 17003, "", 6, 3, 3);
+        checkAndCreateDetector("clusterTest", "192.168.56.105", 17004, "", 6, 3, 3);
+        checkAndCreateDetector("clusterTest", "192.168.56.105", 17005, "", 6, 3, 3);
+        checkAndCreateDetector("clusterTest", "192.168.56.105", 17006, "", 6, 3, 3);
     }
 
-    private RedisServerDetector checkNodes(String testName, String seedHost, Integer seedPort, String password, int allNodeCnt, int masterNodeCnt, int replicaNodeCnt) {
+    private RedisServerDetector checkAndCreateDetector(String testName, String seedHost, Integer seedPort, String password, int allNodeCnt, int masterNodeCnt, int replicaNodeCnt) {
         RedisServerDetector redisServerDetector = new RedisServerDetector(seedHost, seedPort, password);
 
         Set<RedisNode> allNodes = redisServerDetector.getAllNodes();
