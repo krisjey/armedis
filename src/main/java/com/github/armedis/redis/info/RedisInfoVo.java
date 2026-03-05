e71eec0a48384e08b33debc102bae9ab89ed6617
Administrator <Administrator@SKAX25N01331.SKCC.NET>
2026-03-05 20:00:57 +0900

    Reduce parsing log and add todos

--- src/main/java/com/github/armedis/redis/info/RedisInfoVo.java
+++ src/main/java/com/github/armedis/redis/info/RedisInfoVo.java
@@ -280,7 +280,9 @@ public class RedisInfoVo {
                         break;
 
                     default:
-                        logger.error("Can not convert " + sectionName + " " + contentSection);
+                    	// TODO Hotkeys, Keysizes, Threads(io_thread_0:clients=2,reads=407,writes=1179)
+                    	// TODO 데이터가 한건도 없으면 Overview 화면 안나옴.
+                        logger.debug("Does not support " + sectionName + " " + contentSection);
                 }
             }
         }
